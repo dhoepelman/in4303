d2932498cca6118f86355fe9fc800d2760d124e9
David Hoepelman <dhoepelman@gmail.com>
2013-12-10 15:38:11 +0100

    Added execute java strategies

---
+++ MiniJava/editor/java/org/spoofax/lang/minijava/strategies/InteropRegisterer.java
@@ -0,0 +1,17 @@
+package org.spoofax.lang.minijava.strategies;
+
+       import org.strategoxt.lang.JavaInteropRegisterer;
+       import org.strategoxt.lang.Strategy;
+
+       /**
+        * Helper class for {@link java_strategy_0_0}.
+        */
+       public class InteropRegisterer extends JavaInteropRegisterer {
+
+         public InteropRegisterer() {
+           super(new Strategy[] { 
+        		   execute_java_0_1.instance
+           });
+       
+         }
+       }
